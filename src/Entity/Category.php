bd7d1edcd61cc126460cf459a05c5537dd41610e
pavelcodixis <pangelov@codixis.com>
2023-11-14 14:05:26 +0200

    Add admin panel

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -31,7 +31,7 @@ class Category
     /**
      * @var Job[]|ArrayCollection
      *
-     * @ORM\OneToMany(targetEntity="Job", mappedBy="category")
+     * @ORM\OneToMany(targetEntity="Job", mappedBy="category", cascade={"remove"})
      */
     private $jobs;
 
